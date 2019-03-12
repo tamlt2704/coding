07caf6f5ca21b9042ac03e551937dfcecacb2576
tamlt2704 <tamlt2704@gmail.com>
2019-03-12 21:34:50 +0800

    100days alog 02

--- 100days_algorithm/day2_matrix_multiplication.py
+++ 100days_algorithm/day2_matrix_multiplication.py
@@ -1,4 +1,3 @@
-chain = [('A', 10, 20), ('B', 20, 30), ('C', 30, 40)]
 
 
 def mult(chain):
@@ -7,24 +6,26 @@ def mult(chain):
     # init
     aux = {(i,i): (0,) + chain[i] for i in range(n)}
 
-
+    # chain len
     for i in range(1, n):
-
+        # start position of the chain
         for j in range(0, n-i):
 
             best = float('inf')
 
+            # split chain
             for k in range(j, j+i):
 
                 lcost, lname, lrow, lcol = aux[j, k]
                 rcost, rname, rrow, rcol = aux[k+1, j+i]
                 cost = lcost + lrow * lcol * rcol
                 var = '(%s%s)' % (lname, rname)
-
+                
                 if cost < best:
                     aux[j, j+i] = cost, var, lrow, rcol
 
     return dict(zip(['cost', 'order', 'rows', 'cols'], aux[0, n-1]))
 
+chain = [('A', 10, 20), ('B', 20, 30), ('C', 30, 40)]
 print (mult(chain))
 
