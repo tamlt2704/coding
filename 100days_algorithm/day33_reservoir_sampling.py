849d63066c76abf07f40601d6faeeb3c900aa47e
tamlt2704 <tamlt2704@gmail.com>
2019-04-20 22:30:07 +0800

    100days algo 33

---
+++ 100days_algorithm/day33_reservoir_sampling.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def reservoir_sampling(size):
+    i, sample = 0, []
+
+    while True:
+        item = yield i, sample
+
+        i+= 1
+        k = np.random.randint(0, i)
+
+        if len(sample) < size:
+            sample.append(item)
+        elif k < size:
+            sample[k] = item
+reservoir = reservoir_sampling(5)
+print(next(reservoir))
+
+for i in range(1000):
+    k, sample = reservoir.send(i)
+    if k % 100 == 0:
+        print(k, sample)
